87b7a1ddc4929c2d6574958ae6f1ee0889b8ff8b
definiteprogress <85244725+definiteprogress@users.noreply.github.com>
2021-06-03 15:03:10 +0530

    Bootloader amended - Day 2

--- ACH_OS/ach_os/Boot1.asm
+++ ACH_OS/ach_os/Boot1.asm
@@ -32,18 +32,91 @@
 ;	  output - 1+0 records in
 ;         output - 1+0 records out
 ;	  output - 512 bytes copied, 0.00044935 s, 1.1 MB/s
+;      4. bochs - to execute the OS on bochs emulator.
 ;
+;NOTE:
+;       Execute all the commands from the folder that has this file.
 ;*****************************************************************************************************
+bits 16                  ;Indicates that we are still in 16-bit real mode
 
 org 0x7c00               ;BIOS does 0x19 interrupt from its IVT and loads to sector number 0x7c00
                          ;(i.e 1st instruction will be at 0x7C00)
-bits 16                  ;Indicates that we are still in 16-bit real mode
 
-Start:
-        cli              ;Clear all intterupts
-        hlt              ;halts the system
+Start: jmp loader        ;jump over OEM block
 
-times 510 - ($-$$) db 0  ;We have to be 512 bytes. Clear the rest of the bytes with 0.
-                         ;in NASM '$' - represents address of current line,
-                         ;"$$" - address of first instruction (i.e 0x7C00),so current address - 0x7C00
-dw 0xAA55                ;Boot signature
+;*************************************************;
+;	OEM Parameter block
+;*************************************************;
+
+; Error Fix 2 - Removing the ugly TIMES directive -------------------------------------
+
+;;TIMES 0Bh-$+start DB 0                        ; The OEM Parameter Block is exactally 3 bytes
+						; from where we are loaded at. This fills in those
+						; 3 bytes, along with 8 more. Why?
+
+
+bpbOEM			db "ACH OS   "	        ; This member must be exactally 8 bytes. It is just
+						; the name of your OS :) Everything else remains the same.
+
+bpbBytesPerSector:  	DW 512
+bpbSectorsPerCluster: 	DB 1
+bpbReservedSectors: 	DW 1
+bpbNumberOfFATs: 	DB 2
+bpbRootEntries: 	DW 224
+bpbTotalSectors: 	DW 2880
+bpbMedia: 	        DB 0xF0
+bpbSectorsPerFAT: 	DW 9
+bpbSectorsPerTrack: 	DW 18
+bpbHeadsPerCylinder: 	DW 2
+bpbHiddenSectors: 	DD 0
+bpbTotalSectorsBig:     DD 0
+bsDriveNumber: 	        DB 0
+bsUnused: 	        DB 0
+bsExtBootSignature: 	DB 0x29
+bsSerialNumber:	        DD 0xa0a1a2a3
+bsVolumeLabel: 	        DB "MOS FLOPPY "
+bsFileSystem: 	        DB "FAT12   "
+
+msg	db	"Welcome to My Operating System!", 0		; the string to print
+
+;***************************************
+;	Prints a string
+;	DS=>SI: 0 terminated string
+;***************************************
+
+Print:
+	lodsb			; load next byte from string from SI to AL
+	or      al, al		; al=current character
+	jz	PrintDone	; null terminator found? - no,so continue or bail-out
+	mov	ah,	0eh	; get next character
+	int	10h
+	jmp	Print		; Repeat until null terminator found
+PrintDone:
+	ret			; we are done, so return
+
+;*************************************************;
+;	Bootloader Entry Point
+;*************************************************;
+
+loader:
+
+; Error Fix 1 ------------------------------------------
+        xor	ax, ax	; Setup segments to insure they are 0. Remember that
+	mov	ds, ax	; we have ORG 0x7c00. This means all addresses are based
+	mov	es, ax	; from 0x7c00:0. Because the data segments are within the same
+			; code segment, null em.
+
+	mov	si, msg	; our message to print
+	call	Print	; call our print function
+
+	xor	ax, ax	; clear ax
+	int	0x12	; get the amount of KB from the BIOS
+                        ; Now AX = Amount of KB in system recorded by BIOS
+
+        cli             ;Clear all intterupts
+        hlt             ;halts the system
+
+times 510 - ($-$$) db 0 ;We have to be 512 bytes. Clear the rest of the bytes with 0.
+                        ;in NASM '$' - represents address of current line,
+                        ;"$$" - address of first instruction (i.e 0x7C00),so current address - 0x7C00
+dw 0xAA55               ;Boot signature
--- ACH_OS/ach_os/README.md
+++ ACH_OS/ach_os/README.md
@@ -2,6 +2,6 @@
 Operating System developement
 
 Instruction to run os on bochs
-	1. bochs from home directory of project, then select 2.
-	2. Configuration file path : ach_os/bochsrc.txt
-	3. select 6 to simulate os on bochs
+1. bochs from home directory of project, then select 2.
+2. Configuration file path : ach_os/bochsrc.txt
+3. select 6 to simulate os on bochs
